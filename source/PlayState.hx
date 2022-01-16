b29082eab7456ebc7c8822f6cc0702565870307f
BushsHaxs <72326501+BushsHaxs@users.noreply.github.com>
2022-01-16 16:16:36 -0600

    Removed shaders

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -389,11 +389,6 @@ class PlayState extends MusicBeatState
 			gfVersion = SONG.gf;
 		}
 
-		screenshader.waveAmplitude = 1;
-		screenshader.waveFrequency = 2;
-		screenshader.waveSpeed = 1;
-		screenshader.shader.uTime.value[0] = new flixel.math.FlxRandom().float(-100000, 100000);
-
 		var charoffsetx:Float = 0;
 		var charoffsety:Float = 0;
 		if (formoverride == "bf-pixel"
